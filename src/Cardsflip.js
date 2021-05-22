b185c4ed873c4c9b9449efad56bb744d6d267118
inzamam1 <inzamamsayyed10@gmail.com>
2021-05-22 17:20:10 +0530

    Add data to db

--- src/Cardsflip.js
+++ src/Cardsflip.js
@@ -1,17 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Cards.css";
 import "./Circular_menu.css";
+import { db } from "./firebase.js";
 
 function Cardflip({ card }) {
-  const [flip, setFlip] = useState(false);
-
+  const update_water = (event) => {
+    db.collection("posts").doc("2").update({
+      table: "water",
+    });
+  };
+  const update_bill = (event) => {
+    db.collection("posts").doc("2").update({
+      table: "bill",
+    });
+  };
+  const update_waiter = (event) => {
+    db.collection("posts").doc("2").update({
+      table: "waiter",
+    });
+  };
   return (
     <div>
       <div
-        className={`card ${flip ? "flip" : ""}`}
-        onClick={() => {
-          setFlip(!flip);
-        }}
+        className={`card ${card.text.length > 0 ? "flip" : ""}`}
+        onClick={() => {}}
       >
         <div className="front">{card.title}</div>
         <div className="back">{card.text}</div>
@@ -25,13 +37,19 @@ function Cardflip({ card }) {
             <e class="closeBtn"></e>
           </div>
           <div class="btn2">
-            <button class="settings_btn"> water </button>
+            <button onClick={update_water} class="settings_btn">
+              water
+            </button>
           </div>
           <div class="btn2">
-            <button class="settings_btn"> call waiter </button>
+            <button onClick={update_waiter} class="settings_btn">
+              call waiter
+            </button>
           </div>
           <div class="btn2">
-            <button class="settings_btn"> bill </button>
+            <button onClick={update_bill} class="settings_btn">
+              bill
+            </button>
           </div>
         </label>
       </div>
