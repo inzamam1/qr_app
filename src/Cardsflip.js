5d89876a5297a2fd14f02ae4c79193854dd98bd9
inzamam1 <inzamamsayyed10@gmail.com>
2021-05-22 16:14:44 +0530

    Added circular menu

--- src/Cardsflip.js
+++ src/Cardsflip.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Cards.css";
+import "./Circular_menu.css";
 
 function Cardflip({ card }) {
   const [flip, setFlip] = useState(false);
@@ -14,6 +15,25 @@ function Cardflip({ card }) {
       >
         <div className="front">{card.title}</div>
         <div className="back">{card.text}</div>
+      </div>
+
+      <div class="circular_menu">
+        <input type="checkbox" id="toggle" className="checkbox" />
+        <label id="show-menu" for="toggle">
+          <div class="btn2">
+            <e class="menuBtn"></e>
+            <e class="closeBtn"></e>
+          </div>
+          <div class="btn2">
+            <button class="settings_btn"> water </button>
+          </div>
+          <div class="btn2">
+            <button class="settings_btn"> call waiter </button>
+          </div>
+          <div class="btn2">
+            <button class="settings_btn"> bill </button>
+          </div>
+        </label>
       </div>
     </div>
   );
