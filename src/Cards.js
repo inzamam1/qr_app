cf9f7ab2eb9dbb359ee6b8ad485a809a0a690174
inzamam1 <inzamamsayyed10@gmail.com>
2021-06-04 20:09:56 +0530

    Added more tables for testing

--- src/Cards.js
+++ src/Cards.js
@@ -5,12 +5,36 @@ import React, { useState, useEffect } from "react";
 
 function Cards() {
   const [posts, setPosts] = useState([]);
-
+  const [posts1, setPosts1] = useState([]);
+  const [posts2, setPosts2] = useState([]);
+  const [posts3, setPosts3] = useState([]);
+  const [posts4, setPosts4] = useState([]);
+  //Currently set to static tables for trials.
   useEffect(() => {
     db.collection("posts")
       .doc("2")
       .onSnapshot((doc) => {
         setPosts(doc.data().table);
+      });
+    db.collection("posts")
+      .doc("3")
+      .onSnapshot((doc) => {
+        setPosts1(doc.data().table);
+      });
+    db.collection("posts")
+      .doc("4")
+      .onSnapshot((doc) => {
+        setPosts2(doc.data().table);
+      });
+    db.collection("posts")
+      .doc("5")
+      .onSnapshot((doc) => {
+        setPosts3(doc.data().table);
+      });
+    db.collection("posts")
+      .doc("6")
+      .onSnapshot((doc) => {
+        setPosts4(doc.data().table);
       });
   }, []);
 
@@ -18,6 +42,22 @@ function Cards() {
     {
       title: "Table 1",
       text: posts,
+    },
+    {
+      title: "Table 2",
+      text: posts1,
+    },
+    {
+      title: "Table 3",
+      text: posts2,
+    },
+    {
+      title: "Table 4",
+      text: posts3,
+    },
+    {
+      title: "Table 5",
+      text: posts4,
     },
   ];
 
