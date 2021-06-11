2361e557403ff99ccb2c4881ce6105140596e1e7
inzamam1 <inzamamsayyed10@gmail.com>
2021-06-11 19:55:55 +0530

    Removed Loginform.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Loginform from "./Loginform";
 import Home from "./Home.js";
 
 function App() {
@@ -31,9 +30,6 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home}>
             <Home />
-          </Route>
-          <Route path="/">
-            <Loginform />
           </Route>
         </Switch>
       </div>
