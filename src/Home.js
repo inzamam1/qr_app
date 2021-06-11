e94c7b33185eac1fe1c4249bff82db25bc9bc220
inzamam1 <inzamamsayyed10@gmail.com>
2021-06-11 20:52:48 +0530

    cleanup

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,13 @@
-import React, { useState } from "react";
-
+import React from "react";
 import Navbar from "./Navbar";
-import Cards from "./Cards";
 import "./Home.css";
 
 function Home({ user }) {
-  const [posts] = useState([]);
-
   if (navigator.onLine) {
     return (
       <div>
         <Navbar username={user} />
-        <div className="home">
-          <div className={`home_queue${posts.length > 0 ? "" : "_invisible"}`}>
-            {posts.map(({ card, id }) => {
-              return (
-                <div className="object">
-                  <a href={`#${card.id}`}>
-                    <div className="settings_icon" title={card.table}>
-                      <p className="settings_table_no"> {card.id}</p>
-                    </div>
-                  </a>
-                </div>
-              );
-            })}
-          </div>
-          <Cards user={user} />
-        </div>
+        <h1 className="home_queue">This is homepage</h1>
       </div>
     );
   }
